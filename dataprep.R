688b075f5dc72fddc4f81e626115f4e4c1dd2fce
Patrick Carlson <carlson2442@gmail.com>
2020-05-13 16:40:42 -0600

    deployment attempt

---
+++ dataprep.R
@@ -0,0 +1,28 @@
+library(simpleCache) # https://cran.r-project.org/web/packages/simpleCache/vignettes/simpleCacheIntroduction.html
+library(tidyverse)
+library(httr)
+library(jsonlite)
+library(lubridate)
+
+loadDataREST <- function() {
+  # https://covidtracking.com/api
+  request <- httr::GET(url = "https://covidtracking.com/api/v1/states/daily.json")
+  if (request$status_code == "200") {
+    response <- content(request, as="text", encoding="UTF-8")
+    df <- jsonlite::fromJSON(response, flatten=TRUE) %>% 
+      data.frame()
+    
+    df$date <- lubridate::ymd(df$date)
+    df$lastUpdateEt <- lubridate::mdy_hm(df$lastUpdateEt)
+    return(df)
+  }
+}
+
+setCacheDir("./cache")
+if (!file.exists('./cache/covid.RData') || as.numeric(difftime(Sys.time(), file.info('./cache/covid.RData')$mtime, units='hours')) > 6) {
+  simpleCache('covid', loadDataREST(), recreate=TRUE)
+  print("Recreating cache")
+} else {
+  simpleCache('covid')
+  print("Loading data from cache")
+}
--- server.R
+++ server.R
@@ -1,36 +1,12 @@
 library(shiny)
 library(tidyverse)
-library(simpleCache) # https://cran.r-project.org/web/packages/simpleCache/vignettes/simpleCacheIntroduction.html
-library(httr)
-library(jsonlite)
 library(lubridate)
 library(plotly)
 library(shinyWidgets) # https://github.com/dreamRs/shinyWidgets
 library(DT)
 library(visdat)
 
-loadDataREST <- function() {
-  # https://covidtracking.com/api
-  request <- httr::GET(url = "https://covidtracking.com/api/v1/states/daily.json")
-  if (request$status_code == "200") {
-    response <- content(request, as="text", encoding="UTF-8")
-    df <- jsonlite::fromJSON(response, flatten=TRUE) %>% 
-      data.frame()
-    
-    df$date <- lubridate::ymd(df$date)
-    df$lastUpdateEt <- lubridate::mdy_hm(df$lastUpdateEt)
-    return(df)
-  }
-}
-
-setCacheDir("./cache")
-if (!file.exists('./cache/covid.RData') || as.numeric(difftime(Sys.time(), file.info('./cache/covid.RData')$mtime, units='hours')) > 6) {
-  simpleCache('covid', loadDataREST(), recreate=TRUE)
-  print("Recreating cache")
-} else {
-  simpleCache('covid')
-  print("Loading data from cache")
-}
+source(file="dataprep.R")
 
 # https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population
 # Census population estimate July 1, 2019
